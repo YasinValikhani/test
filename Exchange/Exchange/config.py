3c99978ef06f4412cfb5a9cb9d7ab09e3844525d
YasinValikhani <yasinaw22@gmail.com>
2025-08-02 22:44:20 +0330

    jgh

--- Exchange/Exchange/config.py
+++ Exchange/Exchange/config.py
@@ -1,9 +1,3 @@
-# BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
-# API_KEY = 'fbe722d70c2bd3a2842e97c0b2c56671'
-
-# url = BASE_PATH + API_KEY
-
-# config.py
 BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
 API_KEY = 'fbe722d70c2bd3a2842e97c0b2c56671'
 
